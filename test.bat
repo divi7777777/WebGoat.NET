af0a7c1d7ee589a8d9293be75830d18c47041bc4
divi7777777 <96525577+divi7777777@users.noreply.github.com>
2022-01-13 11:19:58 +0530

    Create test.bat

---
+++ test.bat
@@ -0,0 +1,1 @@
+java -jar path/to/ci-driver.jar https://your-enterprise-server:8080 --api-key=secret --site-id=7 --min-severity=high --min-confidence=certain --report-file=scan-report.html --report-type=summary
