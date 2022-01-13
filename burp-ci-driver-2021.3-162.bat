a3cb25292c7f080fe4906140fa1a37e30eec89f2
divi7777777 <96525577+divi7777777@users.noreply.github.com>
2022-01-13 13:30:39 +0530

    Update burp-ci-driver-2021.3-162.bat

--- burp-ci-driver-2021.3-162.bat
+++ burp-ci-driver-2021.3-162.bat
@@ -1,2 +1,2 @@
 @echo off
-start java -jar WebGoat.NET/blob/master/burp-ci-driver-2021.3-162.jar http://localhost:8000 --api-key=cYeFbJHyHb7OVQVWEtEXcT2ekjf9hcjc --site-id=5 --min-severity=high --min-confidence=certain --report-file=scan-report.html --report-type=summary
+start java -jar WebGoat.NET/blob/master/burp-ci-driver-2021.3-162.jar http://127.0.0.1:8000/ --api-key=cYeFbJHyHb7OVQVWEtEXcT2ekjf9hcjc --site-id=5 --min-severity=high --min-confidence=certain --report-file=scan-report.html --report-type=summary
